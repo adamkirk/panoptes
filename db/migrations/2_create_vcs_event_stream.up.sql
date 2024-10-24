2d5a7b8ad6d601132033e33eba140b77278db9a9
Adam Kirk <adam_kirk@live.co.uk>
2024-10-24 19:52:19 +0100

    Setup migrations
    Use go-migrate to handle it all.
    
    Added migrations for the stream tables needed
    
    Added some supporting scripts to make it easier to work with

---
+++ db/migrations/2_create_vcs_event_stream.up.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS vcs_stream(
+   id UUID PRIMARY KEY,
+
+    -- the id of the "aggregate" in the system
+    -- this will depend on whether the event is linked to a pull request for 
+    -- example, where this will be an id for that, or it could be a commit sha etc
+    -- don't think we ever want
+   aggregate_id TEXT NOT NULL,
+
+    -- used for the projection, this controls where it sits in the timeline
+   occurred_at TIMESTAMP (6) WITH TIME ZONE,
+
+    -- when the event was received from the source
+    -- largely useful as meta, and shouldn't affect any projection
+   received_at TIMESTAMP (6) WITH TIME ZONE,
+
+   -- the actual payload for the event
+   -- text because we don't wanna interact with it at a db level, thats what the
+   -- projection and read side are for
+   payload TEXT NOT NULL,
+
+   -- the type of event
+   type TEXT
+);
+
+CREATE INDEX IF NOT EXISTS aggregate_id_idx ON vcs_stream (aggregate_id);
+CREATE INDEX IF NOT EXISTS  occurred_at_idx ON vcs_stream (occurred_at);
+CREATE INDEX IF NOT EXISTS  received_at_idx ON vcs_stream (received_at);
+CREATE INDEX IF NOT EXISTS  type_idx ON vcs_stream (type);
+
+-- aggregate id first as i expect it has higher cardinality than type
+CREATE INDEX IF NOT EXISTS  type_idx ON vcs_stream (aggregate_id, type);
